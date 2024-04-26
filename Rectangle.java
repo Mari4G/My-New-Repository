7fc9c697bf6687dc981b049b32e53d32a8b99db8
MARILINE <fongangmariliane@gmail.com>
2024-04-26 15:02:41 +0100

    Calculate the area of a rectangle

---
+++ Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle{
+    private length;
+    private width;
+}
+public Rectangle(double length, double width){
+    this.length = length;
+    this.width = width;
+}
+public double area(){
+    return length * width;
+}
